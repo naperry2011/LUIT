1035f037755366d28f19ba1ff4cc065448fd670b
Nicholas Perry <nuperry2011@gmail.com>
2024-06-24 03:37:43 -0700

    fix

--- luit_dict.py
+++ luit_dict.py
@@ -19,8 +19,3 @@ spanish_animals = {
 print(spanish_animals['bird'])
 
 
-# Creating Functions. Every function starts off with the 'def' indicator 
-def greet():
-    print('Hello new function!!!')
-    
-greet()    
---
+++ luit_function.py
@@ -0,0 +1,5 @@
+# Creating Functions. Every function starts off with the 'def' indicator 
+def greet():
+    print('Hello new function!!!')
+    
+greet()    
