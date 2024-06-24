3c260fb8088907dca0c0ddbb3a4a7df856ac4132
Nicholas Perry <nuperry2011@gmail.com>
2024-06-24 04:05:35 -0700

    updating

--- luit_function.py
+++ luit_function.py
@@ -2,4 +2,25 @@
 def greet():
     print('Hello new function!!!')
     
-greet()    
+greet() 
+
+
+def get_average(input_numbers):
+    sum = 0.0
+    for number in input_numbers:
+        sum += number
+    average = sum / len(input_numbers)
+    print(average)
+    
+get_average([5, 3, 25, 15, 485, 68])
+
+
+def print_letter_count(text, letter):
+    counter = 0.0
+    for char in text:
+        if char == letter:
+            counter += 1
+    print('Number of', letter, 'is', counter)
+    
+    
+print_letter_count('apple', 'p')
